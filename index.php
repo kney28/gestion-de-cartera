246a921c1158258f48a7decffdb22081b51695a3
kney28 <36701179+kney28@users.noreply.github.com>
2022-08-06 10:32:25 -0500

    Update and rename index.html to index.php

--- index.php
+++ index.php
@@ -5,6 +5,9 @@
 <meta charset="utf-8">
 <title>KSL</title>
 </head>
+  <?php
+  include('include/conexion.php');
+       ?>
 <frameset rows="60,*" cols="*" framespacing="1" frameborder="yes" border="1">
   <frame src="menu.html" name="leftFrame" frameborder="no" scrolling="no" noresize="noresize" marginheight="40" bordercolor="#9900FF" id="leftFrame" title="leftFrame" />
   <frame src="paginaenblanco.html" name="mainFrame" marginwidth="100" marginheight="10" id="mainFrame" title="mainFrame" />
