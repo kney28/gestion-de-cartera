223074335837bbd5c27f7ad0ad772b1cbd0eb7e2
kney28 <36701179+kney28@users.noreply.github.com>
2022-08-06 10:15:32 -0500

    Update conexion.php

--- include/conexion.php
+++ include/conexion.php
@@ -1,5 +1,5 @@
 <?php
-$conexion =mysql_connect("localhost","root","")or die("<p>No se ha podido establecer la conexion con MySQL.</p>");
+/*$conexion =mysql_connect("localhost","root","")or die("<p>No se ha podido establecer la conexion con MySQL.</p>");
 mysql_select_db("cartera",$conexion);
 $tipo_actualizacion=0;
 $documento11="";// esto es para dejar Vacio otra vez el checkbox	
@@ -7,5 +7,13 @@ $b=0;
 $borrado1=NULL;
 $datos=NULL;
 $consul=NULL;
-$t=0;// barra de carga
+$t=0;// barra de carga*/
+
+mysqli_connect(
+string $host= ini_get("containers-us-west-79.railway.app"),
+string $username= ini_get("root"),
+string $passwd= ini_get("pt0FWN1r4EyOpKTqNpVAr"),
+string $dbname= "railway",
+int $port =ini_get("6777")
+): mysqli
 ?>
