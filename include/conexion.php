507a8d57c3574cd7093e5554da12aebea5eb9e31
kney28 <36701179+kney28@users.noreply.github.com>
2022-08-06 10:35:54 -0500

    Update conexion.php

--- include/conexion.php
+++ include/conexion.php
@@ -10,7 +10,7 @@ $consul=NULL;
 $t=0;// barra de carga*/
 
 mysqli_connect(
-string $host= ini_get("containers-us-west-79.railway.app"),
+string $host= "containers-us-west-79.railway.app",
 string $username= ini_get("root"),
 string $passwd= ini_get("pt0FWN1r4EyOpKTqNpVAr"),
 string $dbname= "railway",
